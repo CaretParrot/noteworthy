e65b5665e78814c201199a70f9a0e8334822027e
CaretParrot <rishi.venkatesh720@gmail.com>
2025-11-23 18:36:25 -0500

    Added keyboard shortcuts

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 /**
- * @type {HTMLElement | null}
+ * @type {*}
  */
 let notesElement = document.getElementById("notes");
+let control = false;
+let shift = false;
 
 window.onload = () => {
     if (notesElement === null) {
@@ -14,39 +16,96 @@ window.onload = () => {
 }
 
 function addNote() {
-    let newLine = document.createElement("input");
+    let newLine = document.createElement("span");
     newLine.contentEditable = "true";
-    newLine.placeholder = "...";
+    newLine.innerHTML = ``;
+    newLine.dataset.indent = "0";
 
     newLine.onkeydown = (event) => {
         if (event.key === "Enter") {
+            event.preventDefault();
             addNote();
         }
 
-        if (event.key === "Backspace" && newLine.value === "" && notesElement !== null && notesElement.children.length > 1) {
+        if (event.key === "Backspace") {
+            if (control && notesElement.children.length > 1) {
+                event.preventDefault();
+                deleteNote(newLine);
+            }
+
+            if ((newLine.innerHTML === "<br>" || newLine.innerHTML === "") && notesElement.children.length > 1) {
+                event.preventDefault();
+                deleteNote(newLine);
+            }
+        }
+
+        if (event.key === "Control") {
+            control = true;
+        }
+
+        if (event.key === "Shift") {
+            shift = true;
+        }
+
+        if (event.key === "Tab") {
             event.preventDefault();
-            deleteNote(newLine);
+            newLine.innerHTML += "&nbsp&nbsp&nbsp&nbsp";
+            newLine.innerHTML.replace("<br>", "");
+        }
+
+        if (event.key === "ArrowDown") {
+            if (control) {
+                let text = "";
+                for (let i = 0; i < notesElement.querySelectorAll("*").length; i++) {
+                    text += notesElement.querySelectorAll("*")[i].innerHTML + "\n";
+                }
+                
+                let blob = new Blob([text], { type: "text/plain" });
+                let download = document.createElement("a");
+                download.href = URL.createObjectURL(blob);
+                download.download = "notes.txt";
+                download.click();
+                download.remove();
+            }
         }
 
 
-        if (newLine.value.startsWith("@ ")) {
+        if (newLine.innerHTML.startsWith("@ ")) {
             newLine.style.backgroundColor = "var(--def)";
-        } else if (newLine.value.startsWith("! ")) {
+        } else if (newLine.innerHTML.startsWith("! ")) {
             newLine.style.backgroundColor = "var(--important)";
+        } else if (newLine.innerHTML.startsWith("# ")) {
+            newLine.style.backgroundColor = "var(--heading)";
         } else {
             newLine.style.backgroundColor = "var(--accent)";
         }
     }
-    document.getElementById("notes")?.appendChild(newLine);
+
+    /**
+     * 
+     * @param {*} event 
+     */
+    newLine.onkeyup = (event) => {
+        if (event.key === "Control") {
+            shift = false;
+        }
+
+        if (event.key === "Shift") {
+            shift = false;
+        }
+    }
+
+    newLine.innerHTML.replace("<br>", "");
+    notesElement.appendChild(newLine);
     newLine.focus();
 }
 
 /**
  * 
- * @param {HTMLInputElement} element 
+ * @param {HTMLSpanElement} element 
  */
 function deleteNote(element) {
     element.remove();
-    //@ts-ignore
-    notesElement?.children[notesElement?.children.length - 1].focus();
+    let lastElement = notesElement?.children[notesElement?.children.length - 1];
+    lastElement.focus();
 }
